4d7020f2eefcdbb28696acd894ad83cafbe139bb
sergiy-ta <serhiy819@gmail.com>
2020-11-17 20:57:57 +0200

    add router module

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import http from 'http';
 
+import router from './router';
+
 const app: express.Application = express();
 const port: string | number = process.env.PORT || 3000;
 
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.send("Hello");
-});
+app.use('/', router);
 
 var server = http.createServer(app);
 
